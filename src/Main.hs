6eaed6a854e34eb90197224f9979f47122fcffcd
Mitchell Vitez <mitchell@mercury.co>
2020-12-20 19:34:42 -0500

    use persistent

--- src/Main.hs
+++ src/Main.hs
@@ -1,67 +1,155 @@
-{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
 import Control.Applicative
+import Control.Monad ((<=<))
+import Control.Monad.Except
+import Control.Monad.Logger (LoggingT(..), monadLoggerLog, toLogStr, fromLogStr)
+import Control.Monad.Reader (MonadReader, ask, asks, local)
+import Control.Monad.Trans.Except (runExceptT, ExceptT(..))
+import Control.Monad.Trans.Reader (ReaderT(..))
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
 import Data.Aeson.TH
-import Data.Text
+import Data.Functor.Identity
+import Data.List (intercalate)
+import Data.Pool
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
 import Database.SQLite.Simple
-import Database.SQLite.Simple.FromRow
+import Database.Persist
+import Database.Persist.Sqlite
+  ( acquireSqlConn
+  , createSqlitePool
+  , runMigration
+  , runSqlite
+  , runSqlPool
+  , withSqlConn
+  , ConnectionPool
+  , PersistFieldSql(..)
+  , SqlBackend(..)
+  , SqlPersistT
+  )
+import Database.Persist.TH (mkPersist, mkMigrate, persistLowerCase, share, sqlSettings)
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Say
 import Servant
+import System.Random (randomRIO)
+import System.Random.Pick
 import Util
+import Yesod.Core
 
-dbName = "pianoreps.db"
-port = 1810
+data Config = Config
+  { configPool :: ConnectionPool
+  , configPort :: Port
+  , configDatabaseName :: Text
+  }
 
-data Exercise = Exercise
-  { exerciseBpm :: Int
-  , exerciseName :: Text
-  , exerciseAll12Keys :: Bool
-  } deriving (Eq, Show)
+share [mkPersist sqlSettings, mkMigrate "migrate"] [persistLowerCase|
+Exercise
+  name Text
+  UniqueName name
+  bpm Int
+  hands Hands
+  allKeys Bool
+  deriving Show
+|]
 $(deriveJSON (dropPrefix "exercise") ''Exercise)
-
-instance FromRow Exercise where
-  fromRow = Exercise <$> field <*> field <*> field
-
-instance ToRow Exercise where
-  toRow (Exercise bpm name all12keys) = toRow (bpm, name, all12keys)
 
 type API =
   "list" :> Get '[JSON] [Exercise] :<|>
-  "add" :> ReqBody '[JSON] Exercise :> Post '[JSON] Exercise
+  "add" :> ReqBody '[JSON] Exercise :> Post '[JSON] () :<|>
+  "populate" :> Get '[JSON] Exercise
+
+newtype AppT m a =
+  AppT { runApp :: ReaderT Config (ExceptT ServerError m) a }
+  deriving ( Functor, Applicative, Monad, MonadReader Config, MonadError ServerError, MonadIO)
+
+type App = AppT IO
 
 main :: IO ()
 main = do
-  migrate
-  putStrLn $ "running on port " <> show port
-  run port app
+  withConfig $ \Config{..} -> do
+    application <- initialize Config{..}
+    putStrLn $ "running on port " <> show configPort
+    run configPort application
 
-migrate :: IO ()
-migrate = withConn $ \conn ->
-  execute_ conn "CREATE TABLE IF NOT EXISTS exercises (bpm INTEGER, name TEXT PRIMARY KEY, all12keys BOOLEAN)"
+instance MonadLogger IO where
+  monadLoggerLog loc src lvl msg = do
+    say $ T.pack (show lvl) <> ": " <> (decodeUtf8 . fromLogStr $ toLogStr msg)
 
-addExercise :: Exercise -> Handler Exercise
-addExercise exercise = liftIO $ withConn $ \conn -> do
-  execute conn "INSERT INTO exercises (bpm, name, all12keys) VALUES (?,?,?)" exercise
+initialize :: Config -> IO Application
+initialize Config{..} = do
+  runSqlPool (runMigration migrate) configPool
+  pure $ app Config{..}
+
+withConfig :: (Config -> IO a) -> IO a
+withConfig action = do
+  let dbname = "pianoreps.db"
+  !pool <- createSqlitePool dbname 1
+  action Config
+    { configPool = pool
+    , configPort = 1810
+    , configDatabaseName = dbname
+    }
+
+runDB ::
+  (MonadReader Config m, MonadIO m) =>
+  SqlPersistT IO b -> m b
+runDB query = do
+  pool <- asks configPool
+  liftIO $ runSqlPool query pool
+
+app :: Config -> Application
+app config = serve (Proxy :: Proxy API) $
+  convertApp config exercises :<|>
+  (\ex -> convertApp config (addExercise ex)) :<|>
+  convertApp config populate
+
+convertApp :: Config -> App a -> Handler a
+convertApp config app = do
+  Handler $ runReaderT (runApp app) config
+
+-- Handlers --
+
+addExercise :: Exercise -> App ()
+addExercise exercise =
+  runDB $ insert_ exercise
+
+populate :: App Exercise
+populate = do
+  rand :: Int <- liftIO $ randomRIO (1, 1000)
+  bpm :: Int <- liftIO $ randomRIO (60, 180)
+  hands <- liftIO $ pickOne [LH, RH, HT]
+  all12 <- liftIO $ pickOne [True, False]
+  let exercise = Exercise ("Exercise " <> T.pack (show rand)) bpm hands all12
+  runDB $ insert_ exercise
   pure exercise
 
-withConn :: (Connection -> IO a) -> IO a
-withConn action = do
-  conn <- open dbName
-  a <- action conn
-  close conn
-  pure a
-
-exercises :: Handler [Exercise]
-exercises = liftIO $ withConn $ \conn ->
-  query_ conn "SELECT * FROM exercises"
-
-app :: Application
-app = serve (Proxy :: Proxy API) (exercises :<|> addExercise)
+exercises :: App [Exercise]
+exercises =
+  runDB $ do
+    all <- selectList [] []
+    pure $ map entityVal all
--- src/Util.hs
+++ src/Util.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Util where
 
 import Data.Aeson
+import Data.Aeson.TH
 import Data.Char (toLower)
 import Data.Text (Text)
 import qualified Data.Text as T
+import Database.Persist
+import Database.Persist.Sqlite (runSqlite, runMigration, PersistFieldSql(..))
 
 -- due to template haskell ghc stage constraints, this needs to be in a separate module
 dropPrefix :: Text -> Options
 dropPrefix s = defaultOptions {fieldLabelModifier = map toLower . drop (T.length s)}
+
+-- left, right, or hands together
+data Hands = LH | RH | HT
+  deriving (Eq, Show)
+$(deriveJSON defaultOptions ''Hands)
+
+instance PersistField Hands where
+  toPersistValue = PersistText . T.pack . show
+  fromPersistValue (PersistText hands) = case hands of
+    "LH" -> Right LH
+    "RH" -> Right RH
+    "HT" -> Right HT
+    _ -> Left $ "Invalid hands encoding in database: " <> hands
+  fromPersistValue v = Left $ "Invalid hands format in database: " <> T.pack (show v)
+
+instance PersistFieldSql Hands where
+  sqlType _ = SqlOther "hands"
